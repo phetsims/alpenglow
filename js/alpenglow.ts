9eaf6618a8afeb9de83699adf161cba7fe52d8de
phet-dev <phetadmin@gmail.com>
2023-09-15 03:10:58 -0600

    update copyright dates from daily grunt work

--- js/alpenglow.ts
+++ js/alpenglow.ts
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2023, University of Colorado Boulder
 
 /**
  * The main 'alpenglow' namespace object for the exported (non-Require.js) API. Used internally
