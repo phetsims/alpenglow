e0808689832564f6cb0e59c082633394dcec075e
Jonathan Olson <jonathan.olson@colorado.edu>
2023-09-28 16:35:04 -0600

    Fake corner documentation

--- js/cag/ClippableFace.ts
+++ js/cag/ClippableFace.ts
@@ -3,6 +3,12 @@
 /**
  * An interface for clippable/subdivide-able faces, with defined bounds and area.
  *
+ * NOTE: "fake" corners are needed for some clipping operations (notably the binary line clipping operations, without
+ * bounds). These are corners that are not actually part of the face, but are used when we don't have access to the
+ * entire polygon, and need to output edges which will match up with other results. In these cases, we might generate
+ * edges that go OUTSIDE of the resulting bounds, so if we need to access bounds of the ClippableFace, we'll need to
+ * ignore these "fake" corners.
+ *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
