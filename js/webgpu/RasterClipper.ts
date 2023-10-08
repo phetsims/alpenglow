98a6a40644f6b1cba22b1ee18b6ff47cecd2851b
Jonathan Olson <jonathan.olson@colorado.edu>
2023-10-07 19:46:45 -0600

    Adding note

--- js/webgpu/RasterClipper.ts
+++ js/webgpu/RasterClipper.ts
@@ -220,6 +220,7 @@ export default class RasterClipper {
 
     const context = deviceContext.getCanvasContext( canvas );
 
+    // NOTE: This can change between different frames (swap textures and such)
     const outTexture = context.getCurrentTexture();
 
     const canvasTextureFormat = outTexture.format;
