453cc0b838efbb8a7810bd5308796fb2e5a5ad6a
Jonathan Olson <jonathan.olson@colorado.edu>
2023-10-11 21:19:15 -0600

    RenderInstruction improved binary coding

--- js/render-program/RenderInstruction.ts
+++ js/render-program/RenderInstruction.ts
@@ -29,53 +29,93 @@ export default abstract class RenderInstruction {
 
   // TODO: better code ordering, prevent duplicates
   // length 1
-  public static readonly ReturnCode = 0x00;
-  public static readonly PremultiplyCode = 0x01;
-  public static readonly UnpremultiplyCode = 0x02;
-  public static readonly StackBlendCode = 0x03;
-  public static readonly LinearBlendCode = 0x04;
-  public static readonly LinearDisplayP3ToLinearSRGBCode = 0x05;
-  public static readonly LinearSRGBToLinearDisplayP3Code = 0x06;
-  public static readonly LinearSRGBToOklabCode = 0x07;
-  public static readonly LinearSRGBToSRGBCode = 0x08;
-  public static readonly OklabToLinearSRGBCode = 0x09;
-  public static readonly SRGBToLinearSRGBCode = 0x0a;
-  public static readonly NormalizeCode = 0x0b;
-  public static readonly ExitCode = 0x0c;
-  public static readonly BlendComposeCode = 0x24;
-  public static readonly OpaqueJumpCode = 0x22;
-  public static readonly NormalDebugCode = 0x35;
+  // NOTE: only instructions with length 1 should fit in the mask 0xf (4 bits)
+  // This condition will be detected
+  public static readonly ExitCode = 0x00;
+  public static readonly ReturnCode = 0x01;
+  public static readonly StackBlendCode = 0x02;
+  public static readonly LinearBlendCode = 0x03;
+  public static readonly BlendComposeCode = 0x04;
+  public static readonly OpaqueJumpCode = 0x05;
+  public static readonly PremultiplyCode = 0x06;
+  public static readonly UnpremultiplyCode = 0x07;
+  public static readonly SRGBToLinearSRGBCode = 0x08;
+  public static readonly LinearSRGBToSRGBCode = 0x09;
+  public static readonly LinearDisplayP3ToLinearSRGBCode = 0x0a;
+  public static readonly LinearSRGBToLinearDisplayP3Code = 0x0b;
+  public static readonly OklabToLinearSRGBCode = 0x0c;
+  public static readonly LinearSRGBToOklabCode = 0x0d;
+  public static readonly NormalizeCode = 0x0e;
+  public static readonly NormalDebugCode = 0x0f;
+
+  // Below here, the top bit MUST be set. This is to prevent the 0xf mask from matching these codes.
+  // The bottom 5 bits will be used for the length (FOR NOW, this could change), and the 2 bits after will be used to
+  // represent different instructions.
+  // NOTE: If we have 4 more 1-length instructions, we'll need to figure out a different encoding here
+  // NOTE: Actually, we really won't. For all of those 1-length ones, we can just PACK EVERYTHING IN THE HIGH BITS
+  // NOTE: top 2 bits set will note variadic instructions.
+  // So add... 0x80, 0xa0 as the two options
 
   // length 2
-  public static readonly MultiplyScalarCode = 0x21;
+  public static readonly MultiplyScalarCode = 2 + 0x80;
 
   // length 3
-  public static readonly PhongCode = 0x34;
+  public static readonly PhongCode = 3 + 0x80;
 
   // length 5
-  public static readonly PushCode = 0x20;
+  public static readonly PushCode = 5 + 0x80;
 
   // length 7
-  public static readonly ComputeLinearBlendRatioCode = 0x30;
+  public static readonly ComputeLinearBlendRatioCode = 7 + 0x80;
 
   // length 8
-  public static readonly BarycentricBlendCode = 0x32;
+  public static readonly BarycentricBlendCode = 8 + 0x80;
 
   // length 11
-  public static readonly BarycentricPerspectiveBlendCode = 0x33;
+  public static readonly BarycentricPerspectiveBlendCode = 11 + 0x80;
 
   // length 12
-  public static readonly ComputeRadialBlendRatioCode = 0x31;
+  public static readonly ComputeRadialBlendRatioCode = 12 + 0x80;
 
   // length 21
-  public static readonly FilterCode = 0x36;
+  public static readonly FilterCode = 21 + 0x80;
 
   // variable length(!)
-  public static readonly ComputeLinearGradientRatioCode = 0x37;
-  public static readonly ComputeRadialGradientRatioCode = 0x38;
+  public static readonly ComputeLinearGradientRatioCode = 12 + 0xc0;
+  public static readonly ComputeRadialGradientRatioCode = 10 + 0xc0;
 
-  // TODO
-  public static readonly ImageCode = 0x39;
+  public static readonly ImageCode = 0xff; // TODO: temporary placeholder
+
+  // Something we can pass in shader options.
+  public static readonly CODE_NAME_CONSTANTS = {
+    ExitCode: RenderInstruction.ExitCode,
+    ReturnCode: RenderInstruction.ReturnCode,
+    StackBlendCode: RenderInstruction.StackBlendCode,
+    LinearBlendCode: RenderInstruction.LinearBlendCode,
+    BlendComposeCode: RenderInstruction.BlendComposeCode,
+    OpaqueJumpCode: RenderInstruction.OpaqueJumpCode,
+    PremultiplyCode: RenderInstruction.PremultiplyCode,
+    UnpremultiplyCode: RenderInstruction.UnpremultiplyCode,
+    SRGBToLinearSRGBCode: RenderInstruction.SRGBToLinearSRGBCode,
+    LinearSRGBToSRGBCode: RenderInstruction.LinearSRGBToSRGBCode,
+    LinearDisplayP3ToLinearSRGBCode: RenderInstruction.LinearDisplayP3ToLinearSRGBCode,
+    LinearSRGBToLinearDisplayP3Code: RenderInstruction.LinearSRGBToLinearDisplayP3Code,
+    OklabToLinearSRGBCode: RenderInstruction.OklabToLinearSRGBCode,
+    LinearSRGBToOklabCode: RenderInstruction.LinearSRGBToOklabCode,
+    NormalizeCode: RenderInstruction.NormalizeCode,
+    NormalDebugCode: RenderInstruction.NormalDebugCode,
+    MultiplyScalarCode: RenderInstruction.MultiplyScalarCode,
+    PhongCode: RenderInstruction.PhongCode,
+    PushCode: RenderInstruction.PushCode,
+    ComputeLinearBlendRatioCode: RenderInstruction.ComputeLinearBlendRatioCode,
+    BarycentricBlendCode: RenderInstruction.BarycentricBlendCode,
+    BarycentricPerspectiveBlendCode: RenderInstruction.BarycentricPerspectiveBlendCode,
+    ComputeRadialBlendRatioCode: RenderInstruction.ComputeRadialBlendRatioCode,
+    FilterCode: RenderInstruction.FilterCode,
+    ComputeLinearGradientRatioCode: RenderInstruction.ComputeLinearGradientRatioCode,
+    ComputeRadialGradientRatioCode: RenderInstruction.ComputeRadialGradientRatioCode,
+    ImageCode: RenderInstruction.ImageCode
+  } as const;
 
   /**
    * Returns the length (in dwords) of the binary form of an instruction, based on the initial u32 value in the
@@ -84,57 +124,18 @@ export default abstract class RenderInstruction {
   public static getInstructionLength( u32: number ): number {
     const code = u32 & 0xff;
 
-    switch( code ) {
-      // TODO: consider instead of switch, we could bit mask things like this
-      case RenderInstruction.ReturnCode:
-      case RenderInstruction.PremultiplyCode:
-      case RenderInstruction.UnpremultiplyCode:
-      case RenderInstruction.StackBlendCode:
-      case RenderInstruction.LinearBlendCode:
-      case RenderInstruction.LinearDisplayP3ToLinearSRGBCode:
-      case RenderInstruction.LinearSRGBToLinearDisplayP3Code:
-      case RenderInstruction.LinearSRGBToOklabCode:
-      case RenderInstruction.LinearSRGBToSRGBCode:
-      case RenderInstruction.OklabToLinearSRGBCode:
-      case RenderInstruction.SRGBToLinearSRGBCode:
-      case RenderInstruction.NormalizeCode:
-      case RenderInstruction.ExitCode:
-      case RenderInstruction.BlendComposeCode:
-      case RenderInstruction.OpaqueJumpCode:
-      case RenderInstruction.NormalDebugCode:
-        return 1;
-
-      case RenderInstruction.MultiplyScalarCode:
-        return 2;
-
-      case RenderInstruction.PhongCode:
-        return 3;
-
-      case RenderInstruction.PushCode:
-        return 5;
-
-      case RenderInstruction.ComputeLinearBlendRatioCode:
-        return 7;
-
-      case RenderInstruction.BarycentricBlendCode:
-        return 8;
-
-      case RenderInstruction.BarycentricPerspectiveBlendCode:
-        return 11;
-
-      case RenderInstruction.ComputeRadialBlendRatioCode:
-        return 12;
-
-      case RenderInstruction.FilterCode:
-        return 21;
-
-      case RenderInstruction.ComputeLinearGradientRatioCode:
-        return 12 + 2 * ( u32 >> RenderInstructionComputeGradientRatio.GRADIENT_BEFORE_RATIO_COUNT_BITS );
-      case RenderInstruction.ComputeRadialGradientRatioCode:
-        return 10 + 2 * ( u32 >> RenderInstructionComputeGradientRatio.GRADIENT_BEFORE_RATIO_COUNT_BITS );
-
-      default:
-        throw new Error( `Unknown/unimplemented instruction code: ${code}` );
+    // High 4 bits all zero => 1 length
+    if ( code >> 4 === 0 ) {
+      return 1;
+    }
+    // High 2 bits set => variable length
+    else if ( code & 0xc0 ) {
+      // TODO: factor out a better convention if this becomes more set
+      return code & 0x1f + 2 * ( u32 >> RenderInstructionComputeGradientRatio.GRADIENT_BEFORE_RATIO_COUNT_BITS );
+    }
+    // Just high bit set, we'll read the length in the lower-5 bits
+    else {
+      return code & 0x1f;
     }
   }
 
