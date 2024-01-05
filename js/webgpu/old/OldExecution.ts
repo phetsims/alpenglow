a14b58dae5105e44e05cc59ee9b244a87a12abf0
phet-dev <phetadmin@gmail.com>
2024-01-05 03:10:41 -0700

    update copyright dates from daily grunt work

--- js/webgpu/old/OldExecution.ts
+++ js/webgpu/old/OldExecution.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Helper object for common execution patterns. See methods on DeviceContext for details.
