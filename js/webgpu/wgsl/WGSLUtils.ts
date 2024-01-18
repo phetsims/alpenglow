28c69a9027fab9ecb28f12b972e4065845134715
phet-dev <phetadmin@gmail.com>
2024-01-18 03:09:03 -0700

    update copyright dates from daily grunt work

--- js/webgpu/wgsl/WGSLUtils.ts
+++ js/webgpu/wgsl/WGSLUtils.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Represents a compiled shader and associated data.
--- js/webgpu/wgsl/gpu/logRakedWGSL.ts
+++ js/webgpu/wgsl/gpu/logRakedWGSL.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Specialized logging TODO doc, TODO factor out common parts
