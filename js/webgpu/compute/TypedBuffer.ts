41b6851ea07d28292dee504cc740484c261f231f
phet-dev <phetadmin@gmail.com>
2024-01-26 03:09:51 -0700

    update copyright dates from daily grunt work

--- js/webgpu/compute/TypedBuffer.ts
+++ js/webgpu/compute/TypedBuffer.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * A GPUBuffer that is typed to a specific type, and can be used to encode/decode values to/from it.
