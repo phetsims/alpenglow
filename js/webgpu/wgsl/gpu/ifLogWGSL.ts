83c08a101be7ab8fbeb4c33987285d4b20ff215f
phet-dev <phetadmin@gmail.com>
2024-08-12 03:13:39 -0600

    update copyright dates from daily grunt work

--- js/webgpu/wgsl/gpu/ifLogWGSL.ts
+++ js/webgpu/wgsl/gpu/ifLogWGSL.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2024, University of Colorado Boulder
 
 /**
  * Includes certain WGSL code only if the blueprint has logging enabled.
